1a4d211f3cc354b205610207921782dc74f3f3f5
WolfishZod1 <163739034+WolfishZod1@users.noreply.github.com>
2024-07-31 10:05:52 +0500

    added catalog products

--- src/slices/productsCatalog.slice/productsCatalog.thunks.ts
+++ src/slices/productsCatalog.slice/productsCatalog.thunks.ts
@@ -10,5 +10,43 @@ export const fetchProducts = createAsyncThunk<Product[]>("fetchProducts", async 
             "https://media.dodostatic.net/image/r:233x233/11EF1CB7AB16CECF87C419EF9EEFACA0.avif",
          price: 3150,
       },
+      {
+         name: "Вау! Кебаб",
+         description:
+            "Мясо говядины, соус ранч, сыр моцарелла, сладкий перец, томаты, красный лук и фирменный томатный соус",
+         picture:
+            "https://media.dodostatic.net/image/r:233x233/11EF01FEE6DD7261A9C36187149758D0.avif",
+         price: 2950,
+      },
+      {
+         name: "Креветки со сладким чили",
+         description:
+            "Креветки, ананасы, соус сладкий чили, сладкий перец, моцарелла, фирменный соус альфредо",
+         picture:
+            "https://media.dodostatic.net/image/r:233x233/11EF01FD3C2AC8E791770181C4A9C04D.avif",
+         price: 2950,
+      },
+      {
+         name: "Пицца из половинок",
+         description: "Соберите свою пиццу 35 см с двумя разными вкусами",
+         picture:
+            "https://media.dodostatic.net/image/r:233x233/11EE7D5FECCD3AC0B2E2C417625FCB02.avif",
+         price: 3600,
+      },
+      {
+         name: "Сырная 🌱👶",
+         description: "Моцарелла, сыры чеддер и пармезан, соус альфредо",
+         picture:
+            "https://media.dodostatic.net/image/r:233x233/11EE7D5F837255B58B25A62C60FFDB38.avif",
+         price: 1850,
+      },
+      {
+         name: "Чоризо фреш 🌶",
+         description:
+            "Пикантные колбаски чоризо из цыпленка, зеленый перец, моцарелла, томатный соус",
+         picture:
+            "https://media.dodostatic.net/image/r:233x233/11EE7D5FBD0756A6AAA0BBEBA01B343A.avif",
+         price: 2050,
+      },
    ];
 });
