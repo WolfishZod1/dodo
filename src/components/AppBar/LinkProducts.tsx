e52ffb8f8a134a343915efea23fedbad95abb066
WolfishZod1 <163739034+WolfishZod1@users.noreply.github.com>
2024-08-01 21:18:23 +0500

    anchor links made

--- src/components/AppBar/LinkProducts.tsx
+++ src/components/AppBar/LinkProducts.tsx
@@ -1,4 +1,6 @@
 import { Link, Stack } from "@mui/material";
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import "@components/AppBar/linkProducts.css";
 
 export function LinkProducts() {
    return (
@@ -7,18 +9,43 @@ export function LinkProducts() {
          spacing={2.5}
          sx={{
             width: "896px",
+
+            underline: "none",
          }}
       >
-         <Link>Пиццы</Link>
-         <Link>Комбо</Link>
-         <Link>Закуски</Link>
-         <Link>Коктейли</Link>
-         <Link>Кофе</Link>
-         <Link>Десерты</Link>
-         <Link>Любят дети</Link>
-         <Link>Напитки</Link>
-         <Link>Соусы</Link>
-         <Link>Другие товары</Link>
+         <AnchorLink
+            href="#pizza"
+            style={{ color: "black" }}
+         >
+            <Link>Пиццы</Link>
+         </AnchorLink>
+         <AnchorLink href="#combo">
+            <Link>Комбо</Link>
+         </AnchorLink>
+         <AnchorLink href="#snacks">
+            <Link>Закуски</Link>
+         </AnchorLink>
+         <AnchorLink href="#cocktails">
+            <Link>Коктейли</Link>
+         </AnchorLink>
+         <AnchorLink href="#coffee">
+            <Link>Кофе</Link>
+         </AnchorLink>
+         <AnchorLink href="#dessert">
+            <Link>Десерты</Link>
+         </AnchorLink>
+         <AnchorLink href="#childrenLove">
+            <Link>Любят дети</Link>
+         </AnchorLink>
+         <AnchorLink href="#beverages">
+            <Link>Напитки</Link>
+         </AnchorLink>
+         <AnchorLink href="#sauces">
+            <Link>Соусы</Link>
+         </AnchorLink>
+         <AnchorLink href="#other">
+            <Link>Другие товары</Link>
+         </AnchorLink>
          <Link>Акции</Link>
       </Stack>
    );
